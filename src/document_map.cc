0fe22a94cfb5175e00e82d3be8589f8f5b5df62b
Roman.Khmelichek <Roman.Khmelichek@gmail.com>
2011-01-07 22:35:14 +0000

    Document map for doc URLs and doc sizes. Needs work on compression options and TREC identifier support.

--- src/document_map.cc
+++ src/document_map.cc
@@ -29,4 +29,116 @@
 //==============================================================================================================================================================
 
 #include "document_map.h"
+
+#include <cerrno>
+#include <cstring>
+
+#include <iostream>//TODO
+#include <limits>
+
+#include <fcntl.h>
+#include <strings.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "config_file_properties.h"
+#include "globals.h"
+#include "index_layout_parameters.h"
+#include "logger.h"
 using namespace std;
+
+/**************************************************************************************************************************************************************
+ * DocumentMapReader
+ *
+ **************************************************************************************************************************************************************/
+DocumentMapReader::DocumentMapReader(const char* document_map_filename) :
+  doc_map_fd_(open(document_map_filename, O_RDONLY)), doc_map_buffer_size_(DocMapSize()),
+  doc_map_buffer_(new DocMapEntry[doc_map_buffer_size_]) {
+  int read_ret = read(doc_map_fd_, doc_map_buffer_, doc_map_buffer_size_ * sizeof(*doc_map_buffer_));
+  if (read_ret == -1)
+    assert(false);
+}
+
+DocumentMapReader::~DocumentMapReader() {
+  delete[] doc_map_buffer_;
+  close(doc_map_fd_);
+}
+
+int DocumentMapReader::DocMapSize() {
+  struct stat stat_buf;
+  if (fstat(doc_map_fd_, &stat_buf) < 0) {
+    GetErrorLogger().LogErrno("fstat() in DocumentMapReader::DocMapSize()", errno, true);
+  }
+
+//  cout << "stat_buf.st_size: " << stat_buf.st_size << endl;
+//  cout << "sizeof(*doc_map_buffer_): " << sizeof(*doc_map_buffer_) << endl;
+
+  assert(stat_buf.st_size % sizeof(*doc_map_buffer_) == 0);
+  return stat_buf.st_size / sizeof(*doc_map_buffer_);
+}
+
+/**************************************************************************************************************************************************************
+ * DocumentMapWriter
+ *
+ **************************************************************************************************************************************************************/
+DocumentMapWriter::DocumentMapWriter(const char* document_map_filename_lengths, const char* document_map_filename_urls) :
+    kDocMapBufferSize(2097152),
+    doc_map_fd_lengths_(open(document_map_filename_lengths, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)),
+    doc_map_buffer_(new DocMapEntry[kDocMapBufferSize]), doc_map_buffer_len_(0),
+
+    kUrlsBufferSize(33554432),
+    doc_map_fd_urls_(open(document_map_filename_urls, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)),
+    urls_buffer_(new char[kUrlsBufferSize]), urls_buffer_len_(0) {
+
+}
+
+DocumentMapWriter::~DocumentMapWriter() {
+  DumpBuffer();
+  DumpBufferUrls();
+  delete[] doc_map_buffer_;
+  delete[] urls_buffer_;
+  close(doc_map_fd_lengths_);  //TODO: Check return value.
+  close(doc_map_fd_urls_);  //TODO: Check return value.
+}
+
+void DocumentMapWriter::AddDocMapEntry(const DocMapEntry& doc_map_entry) {
+  assert(doc_map_buffer_len_ < kDocMapBufferSize);
+
+  doc_map_buffer_[doc_map_buffer_len_++] = doc_map_entry;
+  if(doc_map_buffer_len_ == kDocMapBufferSize) {
+    DumpBuffer();
+    doc_map_buffer_len_ = 0;
+  }
+}
+
+void DocumentMapWriter::DumpBuffer() {
+  int write_ret = write(doc_map_fd_lengths_, doc_map_buffer_, sizeof(*doc_map_buffer_) * doc_map_buffer_len_);
+
+  if(write_ret < 0) {
+    GetErrorLogger().Log("write() oops", true);
+  }
+}
+
+void DocumentMapWriter::AddUrl(const char* url, int url_len) {
+  if (urls_buffer_len_ + static_cast<int> (sizeof(url_len)) + url_len > kUrlsBufferSize) {
+    DumpBufferUrls();
+    urls_buffer_len_ = 0;
+  }
+
+  memcpy(urls_buffer_ + urls_buffer_len_, &url_len, sizeof(url_len));
+  urls_buffer_len_ += sizeof(url_len);
+
+  memcpy(urls_buffer_ + urls_buffer_len_, url, url_len);
+  urls_buffer_len_ += url_len;
+}
+
+void DocumentMapWriter::DumpBufferUrls() {
+  int write_ret = write(doc_map_fd_urls_, urls_buffer_, sizeof(*urls_buffer_) * urls_buffer_len_);
+
+  if(write_ret < 0) {
+    GetErrorLogger().Log("write() oops", true);
+  }
+}
--- src/document_map.h
+++ src/document_map.h
@@ -26,28 +26,180 @@
 //==============================================================================================================================================================
 // Author(s): Roman Khmelichek
 //
+//
+/*
+ * TODO: Index reorganization idea:
+ * Lexicon could contain the total number of blocks in the inverted list, so we avoid reading in extra blocks.
+ *
+ * Store all the block headers at the front of the list together at the front of the list.
+ * We can also create a "layered" skipping method by indexing the block headers.
+ * This way, we can first check the block header index, and skip ahead to the correct block header.
+ * We then decompress the block header and find the proper chunk we need to get.
+ *
+ * Another idea to eliminate the "wasted space" at the end of each block. This means that a chunk can span block headers (assuming it can span a maximum of 2 blocks only).
+ * Since we know the size of each chunk (stored in the block header), when we start decompressing a chunk, we know if it'll spill over to the next block.
+ * The PROBLEM with these scheme is that when a chunk spans across a block, we need to get the complete chunk data in a single array. However, our caching scheme (LRU) prevents this
+ * because consecutive blocks are not guaranteed to be placed consecutively in the cache (although it is likely they are). When using this caching scheme, we'd have to check whether
+ * the blocks that the chunk spans are consecutively placed, and if not we need to copy the chunk data into a new array and use it as input to the compression function.
+ * Note that this does not affect us when the index is completely in main memory, since the blocks are always consecutively placed in main memory.
+ */
+//
 // TODO:
 // Have methods for storing fixed length and variable length info.
-// Format: URL_len, URL, Docno_len, Docno, DocLen, DocID
+// Format: URL_len, URL, Docno_len, Docno, DocLen, DocID (Docno is actually a fixed length TREC id)
+//
+// TODO: We're not doing reordering during initial indexing step...yet
 // Docs are not necessarily collected in order, so the docIDs need to be stored.
 //
+// Dynamic fields can be allocated in another big buffer and we'll just store a pointer to it in the fixed buffer. Leads to less memory fragmentation! like PositionsPool
+//
+// The document map is loaded into memory as an array, indexed by the docID.
+// Due to document reordering and possible gaps between the reorded docIDs, our document map may have dummy entries.
+//
+// Notes about the document map:
+// During the initial indexing phase, there are two scenarios:
+// In both scenarios, we don't need the document map in memory. We can simply buffer it and keep appending the data to a single file per index.
+// We can also use integer compression for the integers and character compression for the URLs (check out vcdiff and similar...). This would require us to choose some block
+// size in which to compress these entries.
+// 1) We do document reordering:
+//    This means the document info will not be collected in order (because we'll be remapping it right away to a different docID), so we'll have to store the
+//    remapped docID along with the document info.
+//    Here we can have an optimization: We sort the partial document map buffer by the remapped docIDs prior to dumping it to disk.
+//    So, here we'll have several partial document map files per index, that are now sorted and just need to be merged.
+//    We also have to insert dummy entries into any docID gaps. PROBLEM: We could have very large gaps since we don't control the remapping...might be ok though
+//    Since there might be large gaps. We can elect to store the docIDs within the file, so when we load it, it'll be apparent which docIDs are missing OR
+//    we can just put in dummy entries. Which is more space efficient?
+// 2) We don't do document reordering:
+//    This means that there will not be gaps in the docID and they will be assigned in order. The document map does not need to store the docID. We can just keep appending data
+//    to a single file.
+//
+//
+// TODO: Some ideas:
+//     * When doing an index merge/docID reordering/index layering and similar, we access the document map sequentially per term. So for long lists, we can conserve memory by
+//       reading the document map in blocks of some fixed size.
+//     * We'll need to pre allocate an array for the new full document map in memory because we'll be accessing it all over the place (due to the reordered docs) and we'll just fill it in.
 //==============================================================================================================================================================
 
 #ifndef DOCUMENT_MAP_H_
 #define DOCUMENT_MAP_H_
 
-class DocMapEntry {
+#include <cassert>
+#include <stdint.h>
 
-};
+#include <vector>
 
-class DocumentMap {
+#include "coding_policy.h"
+#include "coding_policy_helper.h"
+#include "configuration.h"
+#include "index_layout_parameters.h"
+
+/**************************************************************************************************************************************************************
+ * DocumentDynamicEntriesPool
+ *
+ **************************************************************************************************************************************************************/
+class DocumentDynamicEntriesPool {
 public:
-  DocumentMap();
+  DocumentDynamicEntriesPool() :
+    size_(0), curr_offset_(0), urls_(NULL) {
+  }
+
+  DocumentDynamicEntriesPool(int size) :
+    size_(size), curr_offset_(0), urls_(new char[size_]) {
+  }
+
+  ~DocumentDynamicEntriesPool() {
+    delete[] urls_;
+  }
+
+  char* StoreUrl(const char* url, int url_len) {
+    assert(urls_ != NULL);
+
+    if (curr_offset_ + url_len < size_) {
+      // TODO: memcpy might be a better choice.
+      for (int i = 0; i < url_len; ++i) {
+        urls_[curr_offset_ + i] = url[i];
+      }
+      curr_offset_ += url_len;
+      return urls_ + (curr_offset_ - url_len);
+    }
+    return NULL;
+  }
+
+  void Reset() {
+    curr_offset_ = 0;
+  }
 
 private:
-  const int kDocMapBufferSize;
-  unsigned char* doc_map_buffer_;
+  int size_;
+  int curr_offset_;
+  char* urls_;
+};
+
+
+struct DocMapEntry {
+  int doc_len;
+//  int doc_url_len;
+//  DocumentDynamicEntriesPool* doc_url;
+};
+
+/**************************************************************************************************************************************************************
+ * DocumentMapReader
+ *
+ * This class reads the complete document map into an in memory buffer for fast access.
+ **************************************************************************************************************************************************************/
+class DocumentMapReader {
+public:
+  DocumentMapReader(const char* document_map_filename);
+  ~DocumentMapReader();
+
+  int size() const {
+    return doc_map_buffer_size_;
+  }
+
+  int DocMapSize();
+
+  int GetDocumentLen(uint32_t doc_id) const {
+    return doc_map_buffer_[doc_id].doc_len;
+  }
+
+private:
+  int doc_map_fd_;
+  int doc_map_buffer_size_;
+  DocMapEntry* doc_map_buffer_;
+};
+
+
+
+/**************************************************************************************************************************************************************
+ * DocumentMapWriter
+ *
+ * This class buffers a portion of the document map before appending it to the document map file.
+ *
+ * In order to use this document map in the case of remapped docIDs, we must store them as well and later resort (do an I/O efficient sort) it by the docID.
+ * Since there may be gaps in the docIDs, we can either store the remapped docIDs along with the document info, or store the docID gaps so that the
+ * DocumentMapReader knows about the gaps. It's not apparent which technique is more space conservative.
+ **************************************************************************************************************************************************************/
+class DocumentMapWriter {
+public:
+  DocumentMapWriter(const char* document_map_filename_lengths, const char* document_map_filename_urls);
+  ~DocumentMapWriter();
+
+  void AddDocMapEntry(const DocMapEntry& doc_map_entry);
+  void DumpBuffer();
+
+  void AddUrl(const char* url, int url_len);
+  void DumpBufferUrls();
+
+private:
+  const int kDocMapBufferSize; // TODO: get from config file
+  int doc_map_fd_lengths_; // TODO might wanna make constant
+  DocMapEntry* doc_map_buffer_;
   int doc_map_buffer_len_;
+
+  const int kUrlsBufferSize;
+  int doc_map_fd_urls_; // TODO might wanna make constant
+  char* urls_buffer_;
+  int urls_buffer_len_;
 };
 
 #endif /* DOCUMENT_MAP_H_ */
