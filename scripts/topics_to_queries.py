3592936e366c68f836e879a3fc88b473fda0edb9
Roman.Khmelichek <Roman.Khmelichek@gmail.com>
2011-02-07 00:28:14 +0000

    Helper script to convert TREC topics to queries.

---
+++ scripts/topics_to_queries.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+'''
+Converts TREC ad hoc and named page retrieval topics to queries usable by
+PolyIRTK when used with the --query-mode=batch-all and --result-format=trec options.
+'''
+import re
+import sys
+
+if len(sys.argv) < 2:
+    sys.exit("Must specify an input file.")
+
+try:
+    topics_file_stream = open(sys.argv[1], 'r')
+except IOError:
+    sys.exit("Couldn't open file '" + sys.argv[1] + "' for reading.")
+
+topics = topics_file_stream.read()
+num_title_pattern = re.compile('<num> Number: (?:NP)?(.*)\\n*<title> (.*)')
+iterator = num_title_pattern.finditer(topics)
+for match in iterator:
+    query = match.group(2).lower()
+    query_pattern = re.compile('\W+')
+    query = query_pattern.sub(' ', query)
+    print(match.group(1) + ":" + query)
